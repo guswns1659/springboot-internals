310903f3d38c6cc5e347d778af4e5703d9da689d
guswns1659 <guswns1659@gmail.com>
2021-10-28 13:49:17 +0900

    feat: Create ConditionalJobConfig

---
+++ src/main/java/com/springboot/springbootinternals/batch/ConditionalJobConfig.java
@@ -0,0 +1,61 @@
+package com.springboot.springbootinternals.batch;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class ConditionalJobConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final MoneyHasDataTasklet moneyHasDataTasklet;
+    private final SimpleTasklet simpleTasklet;
+
+    @Bean
+    public Job conditionalJob() {
+        return jobBuilderFactory
+            .get("conditionalJob")
+            .incrementer(new RunIdIncrementer())
+            .start(moneyHasDataStep())
+            .on("TRUE").to(moneyStep())
+            .from(moneyHasDataStep()).on("FALSE").to(archiveStep())
+            .end()
+            .build();
+    }
+
+    @Bean
+    public Step moneyHasDataStep() {
+        return stepBuilderFactory
+            .get("moneyHasDataStep")
+            .tasklet(moneyHasDataTasklet)
+            .listener(new JackStepListener())
+            .build();
+    }
+
+    @Bean
+    public Step moneyStep() {
+        return stepBuilderFactory
+            .get("moneyStep")
+            .listener(new JackStepListener())
+            .tasklet(simpleTasklet) // 그냥 실행
+            .build();
+    }
+
+    @Bean
+    public Step archiveStep() {
+        return stepBuilderFactory
+            .get("archiveStep")
+            .listener(new JackStepListener())
+            .tasklet(simpleTasklet)
+            .build();
+    }
+}
---
+++ src/main/java/com/springboot/springbootinternals/batch/SimpleTasklet.java
@@ -0,0 +1,19 @@
+package com.springboot.springbootinternals.batch;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class SimpleTasklet implements Tasklet {
+
+    @Override
+    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+        log.info("simple tasklet");
+        return null;
+    }
+}
