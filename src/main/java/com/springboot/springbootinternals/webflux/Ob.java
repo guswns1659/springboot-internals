c3f2f43533331c9f7a88d1aa7632fc0c70a2e471
guswns1659 <guswns1659@gmail.com>
2021-11-07 16:06:28 +0900

    feat: Impl Iterator counting from 1 to 10

--- src/main/java/com/springboot/springbootinternals/webflux/Ob.java
+++ src/main/java/com/springboot/springbootinternals/webflux/Ob.java
@@ -1,15 +1,31 @@
 package com.springboot.springbootinternals.webflux;
 
+import java.util.Iterator;
+
 public class Ob {
 
     /**
-     * 리액티브란? 발생한 이벤트에 대해서 대응하는 방식을 의미한다. duality observer pattern Reactive Stream : 자바를 사용하는 회사들이 만든 리액티브 표준을
-     * 의미한다. java9 API에 들어감.
+     * 리액티브란? 발생한 이벤트에 대해서 대응하는 방식을 의미한다.
+     * duality.
+     * observer pattern.
+     * Reactive Stream : 자바를 사용하는 회사들이 만든 리액티브 표준을 의미한다. java9 API에 들어감.
      */
     public static void main(String[] args) {
-        Iterable<Integer> iterable = () -> {
-                return null;
-            };
+        Iterable<Integer> iterable = () ->
+                new Iterator<>() {
+                    final static int MAX = 10;
+                    int i = 0;
+
+                    @Override
+                    public boolean hasNext() {
+                        return i < MAX;
+                    }
+
+                    @Override
+                    public Integer next() {
+                        return ++i;
+                    }
+                };
 
         for (Integer i : iterable) {
             System.out.println(i);
