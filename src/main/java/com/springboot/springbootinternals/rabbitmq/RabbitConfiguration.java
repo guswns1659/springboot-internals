d6fdd2aecbe352863c0df190232c40015ea7e7bc
guswns1659 <guswns1659@gmail.com>
2021-11-09 14:35:33 +0900

    Rabbit feat: Generate oldRabbitConnectionFactory Bean

--- src/main/java/com/springboot/springbootinternals/rabbitmq/RabbitConfiguration.java
+++ src/main/java/com/springboot/springbootinternals/rabbitmq/RabbitConfiguration.java
@@ -2,9 +2,13 @@ package com.springboot.springbootinternals.rabbitmq;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 @RequiredArgsConstructor
@@ -16,4 +20,24 @@ public class RabbitConfiguration {
     ) {
         return new Jackson2JsonMessageConverter(objectMapper);
     }
+
+    @Bean("oldRabbitConnectionFactory")
+    @Primary
+    public ConnectionFactory oldRabbitConnectionFactory(
+            @Value("${spring.rabbitmq.host}") String host,
+            @Value("${spring.rabbitmq.port}") int port,
+            @Value("${spring.rabbitmq.username}") String username,
+            @Value("${spring.rabbitmq.password}") String password,
+            @Value("${spring.rabbitmq.virtual-host}") String virtualHost
+    ) {
+        CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory();
+        cachingConnectionFactory.setHost(host);
+        cachingConnectionFactory.setPort(port);
+        cachingConnectionFactory.setVirtualHost(virtualHost);
+        cachingConnectionFactory.setUsername(username);
+        cachingConnectionFactory.setPassword(password);
+
+        return cachingConnectionFactory;
+    }
+
 }
