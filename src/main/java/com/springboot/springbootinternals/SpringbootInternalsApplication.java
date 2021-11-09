23d664fe2675f8b7a559b09ae331d015722c5559
guswns1659 <guswns1659@gmail.com>
2021-11-09 18:03:57 +0900

    Rabbit chroe: Exclude Kafka bean register because of rabbitmq test

--- src/main/java/com/springboot/springbootinternals/SpringbootInternalsApplication.java
+++ src/main/java/com/springboot/springbootinternals/SpringbootInternalsApplication.java
@@ -1,9 +1,18 @@
 package com.springboot.springbootinternals;
 
+import com.springboot.springbootinternals.kafka.JackProducer;
+import com.springboot.springbootinternals.kafka.JackProducerConfig;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
+import static org.springframework.context.annotation.ComponentScan.Filter;
+
+@ComponentScan(
+        excludeFilters = @Filter(type = FilterType.ASSIGNABLE_TYPE,
+                classes = {JackProducer.class, JackProducerConfig.class}))
 @SpringBootApplication
 @EnableBatchProcessing
 public class SpringbootInternalsApplication {
