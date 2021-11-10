ca8398a26e3951b0c68ee1c2018163e95fc5ad75
guswns1659 <guswns1659@gmail.com>
2021-11-10 22:49:29 +0900

    Webflux feat: Add multiThreading using ExecutorService

--- src/main/java/com/springboot/springbootinternals/webflux/PubSub.java
+++ src/main/java/com/springboot/springbootinternals/webflux/PubSub.java
@@ -2,11 +2,14 @@ package com.springboot.springbootinternals.webflux;
 
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import static java.util.concurrent.Flow.*;
 
 public class PubSub {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // Publisher == Observable.
         // Subscriber == Observer.
         /**
@@ -17,24 +20,29 @@ public class PubSub {
          */
         Iterable<Integer> itr = Arrays.asList(1, 2, 3, 4, 5);
 
+        ExecutorService es = Executors.newSingleThreadExecutor();
+
         Publisher<Integer> p = new Publisher() {
             @Override
             public void subscribe(Subscriber subscriber) {
                 Iterator<Integer> it = itr.iterator();
                 subscriber.onSubscribe(new Subscription() {
                     public void request(long n) {
-                        try {
-                            while (n-- > 0) {
-                                if (it.hasNext()) {
-                                    subscriber.onNext(it.next());
-                                } else {
-                                    subscriber.onComplete();
-                                    break;
+                        es.execute(() -> {
+                            int i = 0;
+                            try {
+                                while (i++ < n) {
+                                    if (it.hasNext()) {
+                                        subscriber.onNext(it.next());
+                                    } else {
+                                        subscriber.onComplete();
+                                        break;
+                                    }
                                 }
+                            } catch (RuntimeException e) {
+                                subscriber.onError(e);
                             }
-                        } catch (RuntimeException e) {
-                            subscriber.onError(e);
-                        }
+                        });
                     }
 
                     @Override
@@ -51,14 +59,14 @@ public class PubSub {
 
             @Override
             public void onSubscribe(Subscription subscription) {
-                System.out.println("onSubscription");
+                System.out.println(Thread.currentThread().getName() + " onSubscription");
                 this.subscription = subscription;
                 subscription.request(1);
             }
 
             @Override
             public void onNext(Integer item) {
-                System.out.println("onNext " + item);
+                System.out.println(Thread.currentThread().getName() + " onNext " + item);
                 this.subscription.request(1);
             }
 
@@ -74,5 +82,8 @@ public class PubSub {
             }
         };
         p.subscribe(s);
+
+        es.awaitTermination(3, TimeUnit.SECONDS);
+        es.shutdown();
     }
 }
