b0c232d2bd05c5d8af7f5aec6eec95df0e3f13bc
guswns1659 <guswns1659@gmail.com>
2021-11-13 19:25:01 +0900

    Webflux feat: Add map, reduce operator to reactive streams

--- src/main/java/com/springboot/springbootinternals/webflux/ReactorEx.java
+++ src/main/java/com/springboot/springbootinternals/webflux/ReactorEx.java
@@ -4,11 +4,15 @@ import reactor.core.publisher.Flux;
 
 public class ReactorEx {
     public static void main(String[] args) {
-        Flux.create(e -> {
+        Flux.<Integer>create(e -> {
             e.next(1);
             e.next(2);
             e.next(3);
             e.complete();
-        }).log().subscribe(s -> System.out.println(s));
+        }).log()
+                .map(e -> e * 10)
+                .reduce(0, (a,b) -> a+b)
+                .log()
+                .subscribe(s -> System.out.println(s));
     }
 }
