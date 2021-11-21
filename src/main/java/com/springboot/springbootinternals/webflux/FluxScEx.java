8b87f8022bf75186a17aeddd022007f35681de66
guswns1659 <guswns1659@gmail.com>
2021-11-21 18:17:45 +0900

    Webflux feat: Create Flux range publisher for subscribeOn

---
+++ src/main/java/com/springboot/springbootinternals/webflux/FluxScEx.java
@@ -0,0 +1,15 @@
+package com.springboot.springbootinternals.webflux;
+
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+public class FluxScEx {
+    public static void main(String[] args) {
+        Flux.range(1, 10)
+                .log()
+                .subscribeOn(Schedulers.newSingle("sub"))
+                .subscribe(System.out::println);
+
+        System.out.println("exit");
+    }
+}
