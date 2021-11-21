14659ccfd3c056fe13a9ded8f8adac2497e50678
guswns1659 <guswns1659@gmail.com>
2021-11-21 18:36:53 +0900

    Webflux feat: Apply publishOn, subscribeOn to Flux.range

--- src/main/java/com/springboot/springbootinternals/webflux/FluxScEx.java
+++ src/main/java/com/springboot/springbootinternals/webflux/FluxScEx.java
@@ -6,6 +6,7 @@ import reactor.core.scheduler.Schedulers;
 public class FluxScEx {
     public static void main(String[] args) {
         Flux.range(1, 10)
+                .publishOn(Schedulers.newSingle("pub"))
                 .log()
                 .subscribeOn(Schedulers.newSingle("sub"))
                 .subscribe(System.out::println);
