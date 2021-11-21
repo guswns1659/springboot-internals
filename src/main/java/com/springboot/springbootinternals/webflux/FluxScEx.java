a4ef17ce9234179ac0db32ee090ddca8cf63aca6
guswns1659 <guswns1659@gmail.com>
2021-11-21 18:50:06 +0900

    Webflux chore: Add comments mean daemon thread, user thread.

--- src/main/java/com/springboot/springbootinternals/webflux/FluxScEx.java
+++ src/main/java/com/springboot/springbootinternals/webflux/FluxScEx.java
@@ -1,16 +1,22 @@
 package com.springboot.springbootinternals.webflux;
 
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
-import reactor.core.scheduler.Schedulers;
 
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * daemon thread : JVM이 daemon thread만 남으면 종료시킨다.
+ * user thread : JVM이 user thread만 남으면 종료하지 않는다.
+ */
+@Slf4j
 public class FluxScEx {
-    public static void main(String[] args) {
-        Flux.range(1, 10)
-                .publishOn(Schedulers.newSingle("pub"))
-                .log()
-                .subscribeOn(Schedulers.newSingle("sub"))
-                .subscribe(System.out::println);
+    public static void main(String[] args) throws InterruptedException {
+        Flux.interval(Duration.ofMillis(500))
+                .subscribe(s -> log.info("onNext:{}", s));
 
-        System.out.println("exit");
+        log.info("exit");
+        TimeUnit.SECONDS.sleep(5);
     }
 }
