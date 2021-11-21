47e8ee142d77682cd17df7888d6f7c6f93bd7395
guswns1659 <guswns1659@gmail.com>
2021-11-21 18:52:35 +0900

    Webflux feat: Apply take() operator

--- src/main/java/com/springboot/springbootinternals/webflux/FluxScEx.java
+++ src/main/java/com/springboot/springbootinternals/webflux/FluxScEx.java
@@ -9,14 +9,16 @@ import java.util.concurrent.TimeUnit;
 /**
  * daemon thread : JVM이 daemon thread만 남으면 종료시킨다.
  * user thread : JVM이 user thread만 남으면 종료하지 않는다.
+ * -----
+ * take() : 정해진 개수만 받는 operator
  */
 @Slf4j
 public class FluxScEx {
     public static void main(String[] args) throws InterruptedException {
         Flux.interval(Duration.ofMillis(500))
+                .take(10)
                 .subscribe(s -> log.info("onNext:{}", s));
 
-        log.info("exit");
-        TimeUnit.SECONDS.sleep(5);
+        TimeUnit.SECONDS.sleep(10);
     }
 }
