bc6de7252d95559889eb3237dde639fccf0393af
guswns1659 <guswns1659@gmail.com>
2021-12-04 15:36:03 +0900

    javaAsync feat: Comments diff between submit and execute, also callable and runnable.

--- src/main/java/com/springboot/springbootinternals/webflux/FutureEx.java
+++ src/main/java/com/springboot/springbootinternals/webflux/FutureEx.java
@@ -8,14 +8,22 @@ import java.util.concurrent.Executors;
 @Slf4j
 public class FutureEx {
     public static void main(String[] args) throws InterruptedException {
-        // maxium 제한이 없고 처음엔 풀에 스레드가 없다. 사용한 후에 반납하면 그대로 가지고 있다.
+        // newCachedThreadPool = maxium 제한이 없고 처음엔 풀에 스레드가 없다. 사용한 후에 반납하면 그대로 가지고 있다.
         ExecutorService es = Executors.newCachedThreadPool();
 
-        es.execute(() -> {
+        /**
+         * sumbit : have result of async programming.
+         * execute : doesn't have result of async programming.
+         * -------
+         * callable : Ability to return, and throw exception
+         * runnable : Not possible above.
+         */
+        es.submit(() -> {
             try {
                 Thread.sleep(2000);
             } catch (InterruptedException e) {}
             log.info("Async");
+            return "hello";
         });
 
         log.info("Exit");
