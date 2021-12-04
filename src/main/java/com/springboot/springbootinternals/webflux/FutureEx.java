508c6f0b7cdb44881558b197df0a0a1424ad5516
guswns1659 <guswns1659@gmail.com>
2021-12-04 16:19:19 +0900

    javaAsync feat: Apply CallbackFutureTask instead of FutureTask

--- src/main/java/com/springboot/springbootinternals/webflux/FutureEx.java
+++ src/main/java/com/springboot/springbootinternals/webflux/FutureEx.java
@@ -1,51 +1,44 @@
 package com.springboot.springbootinternals.webflux;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
 import java.util.concurrent.*;
 
 @Slf4j
 public class FutureEx {
+    interface SuccessCallback {
+        void onSuccess(String result);
+    }
+
+    public static class CallbackFutureTask extends FutureTask<String> {
+        SuccessCallback sc;
+
+        public CallbackFutureTask(@NotNull Callable<String> callable, SuccessCallback sc) {
+            super(callable);
+            this.sc = Objects.requireNonNull(sc);
+        }
+
+        @SneakyThrows
+        @Override
+        protected void done() {
+            sc.onSuccess(get());
+        }
+    }
+
     public static void main(String[] args) throws InterruptedException, ExecutionException {
         // newCachedThreadPool = maxium 제한이 없고 처음엔 풀에 스레드가 없다. 사용한 후에 반납하면 그대로 가지고 있다.
         ExecutorService es = Executors.newCachedThreadPool();
 
-        /**
-         * sumbit : have result of async programming.
-         * execute : doesn't have result of async programming.
-         * -------
-         * callable : Ability to return, and throw exception
-         * runnable : Not possible above.
-         */
-        Future<String> f = es.submit(new Callable<String>() {
-            @Override
-            public String call() throws Exception {
-                Thread.sleep(2000);
-                log.info("Async");
-                return "hello";
-            }
-        });
-
-        System.out.println(f.isDone());
-        log.info("Exit");
-        Thread.sleep(2100);
-        System.out.println(f.isDone());
-        log.info(f.get());
-
-        System.out.println("----------------------------");
-
-        FutureTask<String> ft = new FutureTask<String>(() -> {
+        CallbackFutureTask cft = new CallbackFutureTask(() -> {
             Thread.sleep(2000);
             log.info("Async");
             return "hello";
-        });
+        }, System.out::println);
 
-        es.execute(ft);
-
-        System.out.println(ft.isDone());
-        log.info("Exit");
-        Thread.sleep(2100);
-        System.out.println(ft.isDone());
-        log.info(ft.get());
+        es.execute(cft);
+        es.shutdown();
     }
 }
