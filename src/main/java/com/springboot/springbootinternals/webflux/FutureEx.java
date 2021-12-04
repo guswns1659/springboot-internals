ba806a45e27602bc696546624a9e125cacf0589c
guswns1659 <guswns1659@gmail.com>
2021-12-04 15:47:26 +0900

    javaAsync feat: Change from execute to submit

--- src/main/java/com/springboot/springbootinternals/webflux/FutureEx.java
+++ src/main/java/com/springboot/springbootinternals/webflux/FutureEx.java
@@ -2,12 +2,11 @@ package com.springboot.springbootinternals.webflux;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 @Slf4j
 public class FutureEx {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         // newCachedThreadPool = maxium 제한이 없고 처음엔 풀에 스레드가 없다. 사용한 후에 반납하면 그대로 가지고 있다.
         ExecutorService es = Executors.newCachedThreadPool();
 
@@ -18,14 +17,19 @@ public class FutureEx {
          * callable : Ability to return, and throw exception
          * runnable : Not possible above.
          */
-        es.submit(() -> {
-            try {
+        Future<String> f = es.submit(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
                 Thread.sleep(2000);
-            } catch (InterruptedException e) {}
-            log.info("Async");
-            return "hello";
+                log.info("Async");
+                return "hello";
+            }
         });
 
+        System.out.println(f.isDone());
         log.info("Exit");
+        Thread.sleep(2000);
+        System.out.println(f.isDone());
+        log.info(f.get());
     }
 }
