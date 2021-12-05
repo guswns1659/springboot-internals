8e3f2fb612cfc32cd03a9704ca3fbbc28a717726
guswns1659 <guswns1659@gmail.com>
2021-12-05 16:26:22 +0900

    javaAsync feat: Apply @Async for async programming in spring

--- src/main/java/com/springboot/springbootinternals/SpringbootInternalsApplication.java
+++ src/main/java/com/springboot/springbootinternals/SpringbootInternalsApplication.java
@@ -2,11 +2,22 @@ package com.springboot.springbootinternals;
 
 import com.springboot.springbootinternals.kafka.JackProducer;
 import com.springboot.springbootinternals.kafka.JackProducerConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.Future;
 
 import static org.springframework.context.annotation.ComponentScan.Filter;
 
@@ -15,10 +26,38 @@ import static org.springframework.context.annotation.ComponentScan.Filter;
                 classes = {JackProducer.class, JackProducerConfig.class}))
 @SpringBootApplication
 @EnableBatchProcessing
+@Slf4j
+@EnableAsync
 public class SpringbootInternalsApplication {
 
+    @Component
+    public static class MyService {
+        // AOP 기반으로 @Async 비동기 작업을 수행한다.
+        @Async
+        public Future<String> hello() throws InterruptedException {
+            log.info("##### hello()");
+            Thread.sleep(2000);
+            return new AsyncResult<>("Hello");
+        }
+
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootInternalsApplication.class, args);
+        try(ConfigurableApplicationContext c = SpringApplication.run(SpringbootInternalsApplication.class, args)) {
+        }
+    }
+
+    @Autowired
+    MyService myService;
+
+    @Bean
+    ApplicationRunner run() {
+        return args -> {
+            log.info("##### run()");
+            Future<String> f = myService.hello();
+            log.info("##### exit " + f.isDone());
+            log.info("result: " + f.get());
+        };
     }
 
 }
