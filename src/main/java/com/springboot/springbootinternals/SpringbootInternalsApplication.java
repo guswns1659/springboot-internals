f4e93527a2880843d78d59c362836646abb52e3a
guswns1659 <guswns1659@gmail.com>
2021-12-09 07:04:45 +0900

    javaAsync feat: Add service dependency while it's running asyncly

--- src/main/java/com/springboot/springbootinternals/SpringbootInternalsApplication.java
+++ src/main/java/com/springboot/springbootinternals/SpringbootInternalsApplication.java
@@ -5,13 +5,19 @@ import com.springboot.springbootinternals.kafka.JackProducerConfig;
 import io.netty.channel.nio.NioEventLoopGroup;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.Netty4ClientHttpRequestFactory;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Service;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -45,7 +51,7 @@ public class SpringbootInternalsApplication {
     AsyncRestTemplate rt = new AsyncRestTemplate(
             new Netty4ClientHttpRequestFactory(new NioEventLoopGroup(1)));
 
-    /** config 셋팅.
+    /** config 셋팅. // thread 하나로 어떻게 동작하는지 알기 위함
      * server:
      *   tomcat:
      *     threads:
@@ -53,6 +59,10 @@ public class SpringbootInternalsApplication {
      */
     @RestController
     public class MyController {
+
+        @Autowired
+        MyService myService;
+
         // success callback을 스프링에서 자동으로 등록해주기 때문에 사용자가 직접 등록하지 않는다.
         @GetMapping("/rest")
         public DeferredResult<String> rest(@RequestParam("idx") int idx) {
@@ -64,7 +74,12 @@ public class SpringbootInternalsApplication {
                 ListenableFuture<ResponseEntity<String>> f2 =
                         rt.getForEntity("http://localhost:8081/service?req={req}", String.class, "hello " + s.getBody());
                 f2.addCallback(s2->{
-                    dr.setResult(s2.getBody());
+                    ListenableFuture<String> f3 = myService.work(s2.getBody());
+                    f3.addCallback(s3-> {
+                        dr.setResult(s3);
+                    }, e->{
+                        dr.setErrorResult(e.getMessage());
+                    });
                 }, e->{
                     dr.setErrorResult(e.getMessage());
                 });
@@ -75,6 +90,23 @@ public class SpringbootInternalsApplication {
 
             return dr;
         }
+    }
+
+    @Service
+    public static class MyService {
+        @Async
+        public ListenableFuture<String> work(String req) {
+            return new AsyncResult<>(req + "/asyncwork");
+        }
+    }
+
+    @Bean
+    public ThreadPoolTaskExecutor myThreadPool() {
+        ThreadPoolTaskExecutor te = new ThreadPoolTaskExecutor();
+        te.setCorePoolSize(1);
+        te.setMaxPoolSize(1);
+        te.initialize();
+        return te;
     }
 
     public static void main(String[] args) {
