7a44b0370476ea86ddfc8e36f3480fcc7c0b64ee
guswns1659 <guswns1659@gmail.com>
2021-12-18 08:23:42 +0900

    chore: Separate main module to webflux, webmvc submodule

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,18 +14,10 @@ allprojects {
     }
 
     dependencies {
-        implementation("org.springframework.boot:spring-boot-starter-web")
-        implementation("org.springframework.boot:spring-boot-starter-webflux")
-        implementation("org.springframework.kafka:spring-kafka")
-        implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-        implementation("com.h2database:h2:1.4.200")
-        implementation("org.springframework.boot:spring-boot-starter-batch:2.5.1")
-        implementation("org.springframework.amqp:spring-rabbit")
+//        implementation("org.springframework.boot:spring-boot-starter-batch:2.5.1")
         // lombok
         implementation("org.projectlombok:lombok")
         annotationProcessor("org.projectlombok:lombok")
-        testImplementation("org.springframework.kafka:spring-kafka-test")
-        testImplementation("org.springframework.boot:spring-boot-starter-test")
     }
     tasks {
         "test"(Test::class) {
@@ -44,5 +36,25 @@ configure(subprojects.filter { it.name == "log4j2" }) {
 
     dependencies {
         implementation("org.springframework.boot:spring-boot-starter-log4j2")
+    }
+}
+
+configure(subprojects.filter {it.name == "webmvc"}) {
+
+    dependencies {
+        implementation("org.springframework.boot:spring-boot-starter-web")
+        implementation("org.springframework.kafka:spring-kafka")
+        implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+        implementation("com.h2database:h2:1.4.200")
+        implementation("org.springframework.amqp:spring-rabbit")
+
+        testImplementation("org.springframework.kafka:spring-kafka-test")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+    }
+}
+
+configure(subprojects.filter {it.name == "webflux"}) {
+    dependencies {
+        implementation("org.springframework.boot:spring-boot-starter-webflux")
     }
 }
