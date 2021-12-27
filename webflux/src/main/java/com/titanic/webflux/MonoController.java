44123c400080118f65eee9a8509f394793c49657
guswns1659 <guswns1659@gmail.com>
2021-12-28 06:56:25 +0900

    Mono feat: Apply subscribe one more before return execution.
    - Add What diff between Cold source and Hot Source.

--- webflux/src/main/java/com/titanic/webflux/MonoController.java
+++ webflux/src/main/java/com/titanic/webflux/MonoController.java
@@ -20,6 +20,14 @@ public class MonoController {
         log.info("pos1");
         // 이 시점이 아니라 return할 때 실행된다. 이유는 구독이 return할 때 발생하기 때문이다.
         Mono<String> m = Mono.fromSupplier(() -> generateHello()).log();
+        /**
+         * Mono, Flux는 하나 이상의 subscriber를 가질 수 있다.
+         * return 전에 subscribe하면 리액티브 스트림이 바로 실행된다.
+         *
+         * Cold source, DB 호출처럼 미리 정해진 코드는 구독할 때마다 처음부터 replay된다.
+         * Hot source, 사용자 입력처럼 미리 정해지지 않는 코드는 구독할 때마다 reploy가 아닌 다음 요청부터 구독한다.
+         */
+        m.subscribe();
         log.info("pos2");
         return m;
     }
