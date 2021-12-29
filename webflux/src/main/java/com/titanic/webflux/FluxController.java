bc2905e15871d72cfce6051f0c91ed86d9ec938b
guswns1659 <guswns1659@gmail.com>
2021-12-30 06:51:38 +0900

    Flux feat: Apply take and delayElements

--- webflux/src/main/java/com/titanic/webflux/FluxController.java
+++ webflux/src/main/java/com/titanic/webflux/FluxController.java
@@ -10,25 +10,33 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/flux")
 public class FluxController {
 
+    /**
+     *  Diff between Mono and Flux
+        // Impossible to operate on each elements of list when Lists exists in Mono.
+        // Possible to operate on each elements of list when Lists exists in Flux.
+     */
+
     @GetMapping("/event/{id}")
     public Mono<List<Event>> event(@PathVariable long id) {
         List<Event> events = Arrays.asList(new Event(1L, "event1"), new Event(2L, "event2"));
-        // Impossible to operate on each elements of list when Lists exists in Mono.
         return Mono.just(events);
     }
 
     @GetMapping(value = "/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<Event> events() {
-        List<Event> events = Arrays.asList(new Event(1L, "event1"), new Event(2L, "event2"));
-        // Possible to operate on each elements of list when Lists exists in Flux.
-        return Flux.fromIterable(events);
+        return Flux
+                .fromStream(Stream.generate(() -> new Event(System.currentTimeMillis(), "value")))
+                .delayElements(Duration.ofSeconds(1)) // parallel thread delay onNext
+                .take(10);
     }
 
     @Data @AllArgsConstructor
