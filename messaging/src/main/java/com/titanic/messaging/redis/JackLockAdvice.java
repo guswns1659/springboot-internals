5234341444d435c355013b65ee509990c9defd43
guswns1659 <guswns1659@gmail.com>
2022-01-05 06:35:51 +0900

    redisLock feat: Resolve redis connection through rerunning docker with port

--- messaging/src/main/java/com/titanic/messaging/redis/JackLockAdvice.java
+++ messaging/src/main/java/com/titanic/messaging/redis/JackLockAdvice.java
@@ -5,27 +5,66 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.data.redis.RedisConnectionFailureException;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 @Aspect
 @Component
 @Slf4j
 public class JackLockAdvice {
 
-    private String getUserIdFromJoinPoint(JoinPoint joinPoint, LockType lockType) {
-        log.info("joinPoint.getArgs() = {}", joinPoint.getArgs());
-        return "1234";
+    private final RedisTemplate<String, String> jackStringRedisTemplate;
+
+    public JackLockAdvice(RedisTemplate<String, String> jackStringRedisTemplate) {
+        this.jackStringRedisTemplate = jackStringRedisTemplate;
+    }
+
+    private String getAccountIdFromUserId(JoinPoint joinPoint, LockType lockType) {
+        Object[] args = joinPoint.getArgs();
+        log.info("joinPoint.getArgs() = {}", args);
+        return String.valueOf(args[0]);
     }
 
     @Around("@annotation(jackLock)")
     public Object JackLockAround(ProceedingJoinPoint joinPoint, JackLock jackLock) throws Throwable {
-        String userId = getUserIdFromJoinPoint(joinPoint, jackLock.value());
+        String accountId = getAccountIdFromUserId(joinPoint, jackLock.value());
 
-        log.info("userId = {}", userId);
-        log.info("Before method call");
-        joinPoint.proceed();
-        log.info("After method call");
+        log.info("accountId = {}", accountId);
 
-        return null;
+        log.info("LOCK start - {}", accountId);
+
+        String key = accountId;
+        String value = String.valueOf(System.currentTimeMillis());
+        Boolean isSuccessLock;
+        try {
+            isSuccessLock = jackStringRedisTemplate.opsForValue().setIfAbsent(key, value);
+
+            if (!isSuccessLock) {
+                if (isExpiredLock(key)) {
+                    jackStringRedisTemplate.delete(key);
+                }
+                log.info("lock fail, accountId - {}", key);
+                throw new RuntimeException();
+            }
+            jackStringRedisTemplate.expire(key, 60, TimeUnit.SECONDS);
+        } catch (RedisConnectionFailureException e) {
+            throw e;
+        }
+
+        log.info("LOCK success - {}", accountId);
+
+        return joinPoint.proceed();
+    }
+
+    private Boolean isExpiredLock(String key) {
+        return Optional.ofNullable(jackStringRedisTemplate.opsForValue().get(key))
+                .map(lastLockAt -> {
+                    long current = System.currentTimeMillis();
+                    return (current - Long.parseLong(lastLockAt)) / 1000 > 60;
+                }).orElse(false);
     }
 }
