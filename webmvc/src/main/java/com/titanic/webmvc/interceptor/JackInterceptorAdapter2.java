17d4257a375d4f689aee3386715ff13c501c4412
guswns1659 <guswns1659@gmail.com>
2022-01-12 06:15:45 +0900

    Interceptor-poc feat: Check order of interceptor with @Order

--- webmvc/src/main/java/com/titanic/webmvc/interceptor/JackInterceptorAdapter2.java
+++ webmvc/src/main/java/com/titanic/webmvc/interceptor/JackInterceptorAdapter2.java
@@ -3,22 +3,19 @@ package com.titanic.webmvc.interceptor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @RequiredArgsConstructor
 @Slf4j
-@Order(Ordered.HIGHEST_PRECEDENCE)
-public class JackInterceptorAdapter extends WebMvcConfigurerAdapter {
+public class JackInterceptorAdapter2 extends WebMvcConfigurerAdapter {
 
-    private final JackHandlerInterceptor jackHandlerInterceptor;
+    private final JackHandlerInterceptor2 jackHandlerInterceptor2;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        log.info("JackInterceptorAdapter");
-        registry.addInterceptor(jackHandlerInterceptor);
+        log.info("JackInterceptorAdapter2");
+        registry.addInterceptor(jackHandlerInterceptor2);
     }
 }
