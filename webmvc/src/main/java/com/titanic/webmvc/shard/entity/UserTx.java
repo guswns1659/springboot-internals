483ec7b73d10f489fb856438c04295a38f27a505
guswns1659 <guswns1659@gmail.com>
2022-01-12 06:50:49 +0900

    Mysql-shard feat: Rename ShardTx to UserTx

--- webmvc/src/main/java/com/titanic/webmvc/shard/entity/UserTx.java
+++ webmvc/src/main/java/com/titanic/webmvc/shard/entity/UserTx.java
@@ -6,9 +6,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class ShardTx {
+public class UserTx {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String name;
 }
