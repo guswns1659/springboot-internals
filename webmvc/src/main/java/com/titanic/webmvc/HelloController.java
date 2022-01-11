d8b26bb906b72a7de79094bb595de16584388007
guswns1659 <guswns1659@gmail.com>
2022-01-12 07:35:39 +0900

    Mysql-shard feat: Create UserTxService

--- webmvc/src/main/java/com/titanic/webmvc/HelloController.java
+++ webmvc/src/main/java/com/titanic/webmvc/HelloController.java
@@ -1,13 +1,24 @@
 package com.titanic.webmvc;
 
+import com.titanic.webmvc.shard.service.UserTxService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class HelloController {
+
+    private final UserTxService userTxService;
 
     @GetMapping("hello")
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/user/{userId}/{type}")
+    public String user(@PathVariable("userId") Long userId, @PathVariable("type") String type) {
+        return userTxService.save(userId, type);
     }
 }
--- webmvc/src/main/java/com/titanic/webmvc/shard/entity/UserTx.java
+++ webmvc/src/main/java/com/titanic/webmvc/shard/entity/UserTx.java
@@ -1,5 +1,7 @@
 package com.titanic.webmvc.shard.entity;
 
+import lombok.Builder;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,5 +14,14 @@ public class UserTx {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
+    private String type;
+
+    public UserTx() {
+    }
+
+    @Builder
+    public UserTx(Long id, String type) {
+        this.id = id;
+        this.type = type;
+    }
 }
