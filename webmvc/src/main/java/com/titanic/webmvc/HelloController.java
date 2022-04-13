c6532723899634076482c5dc66460dc4c01475ce
guswns1659 <guswns1659@gmail.com>
2022-04-13 22:52:34 +0900

    test: jackson 라이브러리 테스트

--- webmvc/src/main/java/com/titanic/webmvc/HelloController.java
+++ webmvc/src/main/java/com/titanic/webmvc/HelloController.java
@@ -1,9 +1,12 @@
 package com.titanic.webmvc;
 
 import com.titanic.webmvc.shard.service.UserTxService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,5 +23,37 @@ public class HelloController {
     @GetMapping("/user/{userId}/{type}")
     public String user(@PathVariable("userId") Long userId, @PathVariable("type") String type) {
         return userTxService.save(userId, type);
+    }
+
+    @GetMapping("/jackson")
+    public ResponseDto jackson() {
+        return new ResponseDto("name", "age");
+    }
+
+    @PostMapping("/jackson/request")
+    public ResponseDto jacksonRequest(@RequestBody RequestDto requestDto) {
+        return new ResponseDto("name", "age");
+    }
+}
+
+class RequestDto {
+    private String name;
+
+    private RequestDto() {
+    }
+
+    public RequestDto(String name) {
+        this.name = name;
+    }
+}
+
+@Getter
+class ResponseDto {
+    private String name;
+    private String age;
+
+    public ResponseDto(String name, String age) {
+        this.name = name;
+        this.age = age;
     }
 }
