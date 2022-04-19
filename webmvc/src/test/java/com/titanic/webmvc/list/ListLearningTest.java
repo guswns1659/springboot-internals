4eaa532422f17e0003c4aaeb8e9034e3907854f6
guswns1659 <guswns1659@gmail.com>
2022-04-19 17:31:59 +0900

    test: List에 null 넣었을 때 Size 증가하는 학습테스트

---
+++ webmvc/src/test/java/com/titanic/webmvc/list/ListLearningTest.java
@@ -0,0 +1,17 @@
+package com.titanic.webmvc.list;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class ListLearningTest {
+
+    @Test
+    void nullGiven_ShouldIncreaseListSize() {
+        List<String> strings = new ArrayList<>();
+        strings.add(null);
+        assertThat(strings.size()).isEqualTo(1);
+    }
+}
