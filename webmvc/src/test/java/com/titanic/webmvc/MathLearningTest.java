db1288bf74bb2266714a56d17c27d665de9138c7
guswns1659 <guswns1659@gmail.com>
2022-04-21 06:32:32 +0900

    test: Math.toIntExact 학습테스트

--- webmvc/src/test/java/com/titanic/webmvc/MathLearningTest.java
+++ webmvc/src/test/java/com/titanic/webmvc/MathLearningTest.java
@@ -1,6 +1,7 @@
 package com.titanic.webmvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 
@@ -14,4 +15,11 @@ public class MathLearningTest {
         assertThat(Math.round(value2)).isEqualTo(124);
     }
 
+    @Test
+    void toIntExact() {
+        assertThatThrownBy(() -> Math.toIntExact(Long.MAX_VALUE))
+            .isInstanceOf(ArithmeticException.class);
+
+        assertThat(Math.toIntExact(1234L)).isEqualTo(1234);
+    }
 }
