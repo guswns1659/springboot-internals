dc9c3821ea57b0342c2a6fe0c6f66311ea1795f7
guswns1659 <guswns1659@gmail.com>
2022-09-15 21:20:37 +0900

    feat: jdbc 이용한 select, update, delete 실습

--- springdb/src/main/java/com/titanic/springdb/repository/MemberRepositoryV0.java
+++ springdb/src/main/java/com/titanic/springdb/repository/MemberRepositoryV0.java
@@ -8,6 +8,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 @Slf4j
 public class MemberRepositoryV0 {
@@ -28,6 +29,79 @@ public class MemberRepositoryV0 {
             pstmt.executeUpdate();
         } catch (SQLException e) {
             log.error("db save error", e);
+        } finally {
+            close(con, pstmt, null);
+        }
+    }
+
+    public Member findById(String id) throws SQLException {
+
+        String sql = "select * from member where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            con = DBConnectionUtils.getConnection();
+
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, id);
+
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                String memberId = rs.getString("member_id");
+                int money = rs.getInt("money");
+                return new Member(memberId, money);
+            } else {
+                throw new NoSuchElementException("member not found, memberId = {}" + id);
+            }
+
+        } catch (SQLException e) {
+            log.error("findById error", e);
+            throw e;
+        } finally {
+            close(con, pstmt, rs);
+        }
+    }
+
+    public void update(String memberId, int money) throws SQLException {
+        String sql = "update member set money = ? where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        try {
+            con = DBConnectionUtils.getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1, money);
+            pstmt.setString(2, memberId);
+            int resultSize = pstmt.executeUpdate();
+            log.info("resultSize = {}", resultSize);
+        } catch (SQLException e) {
+            log.error("update error", e);
+            throw e;
+        } finally {
+            close(con, pstmt, null);
+        }
+
+    }
+
+    public int delete(String memberId) throws SQLException {
+        String sql = "delete from member where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = DBConnectionUtils.getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+            int resultSize = pstmt.executeUpdate();
+            log.info("resultSize = {}", resultSize);
+            return resultSize;
+        } catch (SQLException e) {
+            log.error("delete error", e);
+            throw e;
         } finally {
             close(con, pstmt, null);
         }
--- springdb/src/test/java/com/titanic/springdb/MemberRepositoryV0Test.java
+++ springdb/src/test/java/com/titanic/springdb/MemberRepositoryV0Test.java
@@ -2,14 +2,45 @@ package com.titanic.springdb;
 
 import com.titanic.springdb.model.Member;
 import com.titanic.springdb.repository.MemberRepositoryV0;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@Slf4j
 class MemberRepositoryV0Test {
+    private static final String MEMBERID = "MEMBER1";
+    private static final int money = 10000;
+    private static final int newMoney = 20000;
+
     private MemberRepositoryV0 memberRepository = new MemberRepositoryV0();
 
     @Test
-    void save() {
-        // when
-        memberRepository.save(new Member("MEMBER1", 10000));
+    void crud() throws SQLException {
+        // create
+        Member member = new Member(MEMBERID, money);
+        memberRepository.save(member);
+
+        // read
+        Member findMember = memberRepository.findById(MEMBERID);
+        log.info("findMember = {}", findMember);
+
+        assertThat(findMember).isEqualTo(member);
+
+        // update
+        memberRepository.update(findMember.getMemberId(), newMoney);
+        Member updatedMember = memberRepository.findById(findMember.getMemberId());
+        log.info("updatedMember = {}", updatedMember);
+
+        assertThat(updatedMember.getMoney()).isEqualTo(newMoney);
+
+        // delete
+        memberRepository.delete(updatedMember.getMemberId());
+        assertThatThrownBy(() -> memberRepository.findById(updatedMember.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
     }
 }
