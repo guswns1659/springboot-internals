b47dd474461ff3bf27000a9517d4130ec288f932
guswns1659 <guswns1659@gmail.com>
2023-01-23 17:50:22 +0900

    chore: 4장, V2 빈 생성

--- spring-advanced-lecture/two/src/main/java/com/younghan/two/TwoApplication.java
+++ spring-advanced-lecture/two/src/main/java/com/younghan/two/TwoApplication.java
@@ -1,11 +1,12 @@
 package com.younghan.two;
 
 import com.younghan.two.proxy.config.AppV1Config;
+import com.younghan.two.proxy.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(AppV1Config.class)
+@Import({AppV1Config.class, AppV2Config.class})
 @SpringBootApplication(scanBasePackages = "com.younghan.two.proxy.app")
 public class TwoApplication {
 
