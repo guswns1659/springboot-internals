bf351259dd0e667a0eda91bdbd96dcf635f7e474
guswns1659 <guswns1659@gmail.com>
2023-01-23 17:07:01 +0900

    chore: 4장, 인터페이스있는 클래스 셋팅

--- logtrace/src/main/java/com/logtrace/logtrace/LogtraceApplication.java
+++ logtrace/src/main/java/com/logtrace/logtrace/LogtraceApplication.java
@@ -1,9 +1,12 @@
 package com.logtrace.logtrace;
 
+import com.logtrace.logtrace.proxy.config.AppV1Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@Import(AppV1Config.class)
+@SpringBootApplication(scanBasePackages = "com.logtrace.logtrace.proxy.app")
 public class LogtraceApplication {
 
     public static void main(String[] args) {
