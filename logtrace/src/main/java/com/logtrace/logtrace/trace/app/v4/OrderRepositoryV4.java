08002be9a2786870eeb1ed8990b8d2ac5b4d6799
guswns1659 <guswns1659@gmail.com>
2023-01-23 15:48:15 +0900

    feat: 템플릿메서드 패턴 적용해 변하지 않는 코드 한 곳에 분리

---
+++ logtrace/src/main/java/com/logtrace/logtrace/trace/app/v4/OrderRepositoryV4.java
@@ -0,0 +1,36 @@
+package com.logtrace.logtrace.trace.app.v4;
+
+import com.logtrace.logtrace.logtrace.LogTrace;
+import com.logtrace.logtrace.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV4 {
+    private final LogTrace trace;
+
+    public void save(String itemId) {
+        AbstractTemplate<Void> abstractTemplate = new AbstractTemplate<>(trace) {
+
+            @Override
+            protected Void call() {
+                if (itemId.equals("ex")) {
+                    throw new IllegalStateException("예외 발생!");
+                }
+                sleep(1000);
+                return null;
+            }
+        };
+
+        abstractTemplate.execute("OrderRepository.save()");
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
