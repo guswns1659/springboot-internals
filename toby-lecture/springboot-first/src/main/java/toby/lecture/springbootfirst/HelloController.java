8f5440cdc917330635781fc49be581d43b7af792
guswns1659 <guswns1659@gmail.com>
2023-02-26 17:19:15 +0900

    [스프링부트 - 이해와 원리] 3장 : 독립 실행형 서블릿 애플리케이션 실습

---
+++ toby-lecture/springboot-first/src/main/java/toby/lecture/springbootfirst/HelloController.java
@@ -0,0 +1,8 @@
+package toby.lecture.springbootfirst;
+
+public class HelloController {
+
+    public String hello(String name) {
+        return "Hello " + name;
+    }
+}
--- toby-lecture/springboot-first/src/main/java/toby/lecture/springbootfirst/SpringbootFirstApplication.java
+++ toby-lecture/springboot-first/src/main/java/toby/lecture/springbootfirst/SpringbootFirstApplication.java
@@ -1,13 +1,42 @@
 package toby.lecture.springbootfirst;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
-@SpringBootApplication
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class SpringbootFirstApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootFirstApplication.class, args);
-	}
+		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 
+		WebServer webServer = serverFactory.getWebServer(servletContext -> servletContext.addServlet("hello", new HttpServlet() {
+			@Override
+			protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+				if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+					String name = req.getParameter("name");
+
+					HelloController helloController = new HelloController();
+					String res = helloController.hello(name);
+
+					resp.setStatus(HttpStatus.OK.value());
+					resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+					resp.getWriter().println(res);
+				}  else {
+					resp.setStatus(HttpStatus.NOT_FOUND.value());
+				}
+			}
+		}).addMapping("/"));
+
+		webServer.start();
+	}
 }
