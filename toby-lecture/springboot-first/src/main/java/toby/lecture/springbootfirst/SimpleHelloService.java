a7c0ae6ee365e7ec28c5d61818350a77f73cb47e
guswns1659 <guswns1659@gmail.com>
2023-02-26 17:52:40 +0900

    [스프링부트 - 이해와 원리] 4장 : @ComponentScan 적용

--- toby-lecture/springboot-first/src/main/java/toby/lecture/springbootfirst/SimpleHelloService.java
+++ toby-lecture/springboot-first/src/main/java/toby/lecture/springbootfirst/SimpleHelloService.java
@@ -1,5 +1,8 @@
 package toby.lecture.springbootfirst;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class SimpleHelloService implements HelloService{
     @Override
     public String sayHello(String name) {
