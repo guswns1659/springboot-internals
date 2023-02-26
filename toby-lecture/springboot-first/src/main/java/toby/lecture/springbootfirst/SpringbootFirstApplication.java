b39d6f3f0ad90fba6cfb2bf757f8f233afc80240
guswns1659 <guswns1659@gmail.com>
2023-02-26 17:48:24 +0900

    [스프링부트 - 이해와 원리] 4장 : 자바 구성 정보 이용해 빈 초기화 진행
    - AnnotationConfigWebApplicationContext로 변경

--- toby-lecture/springboot-first/src/main/java/toby/lecture/springbootfirst/SpringbootFirstApplication.java
+++ toby-lecture/springboot-first/src/main/java/toby/lecture/springbootfirst/SpringbootFirstApplication.java
@@ -2,13 +2,26 @@ package toby.lecture.springbootfirst;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+@Configuration
 public class SpringbootFirstApplication {
 
+    @Bean
+    public HelloController helloController(HelloService helloService) {
+        return new HelloController(helloService);
+    }
+
+    @Bean
+    public HelloService helloService() {
+        return new SimpleHelloService();
+    }
+
     public static void main(String[] args) {
-        GenericWebApplicationContext context = new GenericWebApplicationContext() {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext() {
             @Override
             protected void onRefresh() {
                 super.onRefresh();
@@ -21,8 +34,7 @@ public class SpringbootFirstApplication {
                 webServer.start();
             }
         };
-        context.registerBean(HelloController.class);
-        context.registerBean(SimpleHelloService.class);
+        context.register(SpringbootFirstApplication.class);
         context.refresh();
     }
 }
