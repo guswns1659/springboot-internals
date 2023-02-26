f0cf5d9126dc87dc01be71bdb84b5c74983c6f0f
guswns1659 <guswns1659@gmail.com>
2023-02-26 18:12:43 +0900

    [스프링부트 - 이해와 원리] 4장 : MySpringApplication으로 스프링 부트 초기 코드 흉내내기

--- toby-lecture/springboot-first/src/main/java/toby/lecture/springbootfirst/SpringbootFirstApplication.java
+++ toby-lecture/springboot-first/src/main/java/toby/lecture/springbootfirst/SpringbootFirstApplication.java
@@ -1,31 +1,27 @@
 package toby.lecture.springbootfirst;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
 @ComponentScan
 public class SpringbootFirstApplication {
 
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+
+    @Bean
+    public ServletWebServerFactory serverFactory() {
+        return new TomcatServletWebServerFactory();
+    }
+
     public static void main(String[] args) {
-        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext() {
-            @Override
-            protected void onRefresh() {
-                super.onRefresh();
-
-                TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-
-                WebServer webServer = serverFactory.getWebServer(servletContext ->
-                        servletContext.addServlet("dispatcherServlet", new DispatcherServlet(this)).addMapping("/"));
-
-                webServer.start();
-            }
-        };
-        context.register(SpringbootFirstApplication.class);
-        context.refresh();
+        MySpringApplication.run(SpringbootFirstApplication.class, args);
     }
 }
